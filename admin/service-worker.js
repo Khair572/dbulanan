3015916704132c4f2b43800c8cf00fc63d297ac2
HAIHEX <regongithub572@gmail.com>
2026-09-26 17:32:54 +0800

    Delete admin/service-worker.js

--- admin/service-worker.js
+++
@@ -1,7 +0,0 @@
-self.addEventListener('install', (e) => {
-  console.log('Service Worker (admin) terpasang');
-});
-
-self.addEventListener('fetch', (e) => {
-  // Biarkan kosong agar website tetap berjalan normal secara online
-});
